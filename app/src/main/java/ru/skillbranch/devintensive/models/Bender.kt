13558289e7cf1ff354d9669af7abf10e46fe10fd
Semen Dubina <semen@sam002.net>
2019-12-30 23:19:33 +0300

    Fix colors

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -35,10 +35,10 @@ class Bender(var status:Status = Status.NORMAL, var question: Question = Questio
     }
 
     enum class Status(val color: Triple<Int,Int,Int>){
-        NORMAL(Triple(255,255,255)),
-        WARNING(Triple(255,120,0)),
-        DANGER(Triple(255,60,60)),
-        CRITICAL(Triple(255,255,0));
+        NORMAL(Triple(255, 255, 255)) ,
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0)) ;
 
         open fun nextStatus():Status {
             return if(this.ordinal< values().lastIndex) {
